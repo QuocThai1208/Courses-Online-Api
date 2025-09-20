05fb6ba6110b590c0dc4f1d1601ed8b497d661bc
Trần Quang Trường <128164053+QuangTruongPractice@users.noreply.github.com>
2025-09-20 17:58:30 +0700

    Update views.py

--- courses/views.py
+++ courses/views.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 import hmac, hashlib
-from courses.models import Category, Course, User, Role, UserCourse, Forum, Comment, Chapter, Lesson
+from courses.models import Category, Course, User, Role, UserCourse, Forum, Comment, Chapter, Lesson, CourseStatus
 from courses import serializers, paginators
 from .perms import IsAdmin, IsStudent, IsTeacher, IsTeacherOrAdmin
 from .services.momo import create_momo_payment, update_status_user_course
